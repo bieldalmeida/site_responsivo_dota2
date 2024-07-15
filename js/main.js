20de22c639ef6711b559d20dc8e7d981435919f9
Gabriel D'Almeida <gabriel.dalmeida@outlook.com>
2024-07-15 10:17:45 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('.menu-hamburguer').click(function() {
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            telefone: 'Por favor, insira o seu telefone',
+            email: 'Por favor, insira o seu e-mail'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
